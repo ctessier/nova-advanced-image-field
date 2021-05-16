d10927269673a3f9f257b33c9ddd2403e4f87e98
Clément Tessier <clement.tessier@ymail.com>
2021-05-16 11:25:00 +0200

    chore: remove useless css

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
    .js('resources/js/field.js', 'js')
-   .sass('resources/sass/field.scss', 'css')
    .webpackConfig({
       resolve: {
           alias: {
