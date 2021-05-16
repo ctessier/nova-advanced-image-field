bcef22a6ade28c07f5a3efc5854a41269fbfe4f8
Clément Tessier <clement.tessier@ymail.com>
2021-05-16 11:36:39 +0200

    remove useless attribute

--- src/AdvancedAvatar.php
+++ src/AdvancedAvatar.php
@@ -6,10 +6,5 @@ use Laravel\Nova\Contracts\Cover;
 
 class AdvancedAvatar extends AdvancedImage implements Cover
 {
-    /**
-     * Indicates whether the image should be fully rounded or not.
-     *
-     * @var bool
-     */
-    public $rounded = true;
+    //
 }
