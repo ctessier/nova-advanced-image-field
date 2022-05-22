a318ce4dbfa69231aa40c532c35ff4881be79334
Clément Tessier <ctessier@users.noreply.github.com>
2022-05-22 10:35:53 +0200

    Fix StyleCI issue

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Ctessier\NovaAdvancedImageField;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
