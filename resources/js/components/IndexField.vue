4399191dd5a493fa84f99b778e080c65ac25646b
Clément Tessier <clement.tessier@ymail.com>
2022-11-29 21:46:15 +0100

    Use Nova file field vue files for detail and index

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,19 +1,56 @@
 <template>
-    <p>
-        <img
-            v-if="field.thumbnailUrl"
-            :src="field.thumbnailUrl"
-            style="object-fit: cover;"
-            class="w-8 h-8"
-            :class="{ 'rounded-full': field.rounded, rounded: !field.rounded }"
-            :alt="field.name"
-        />
-        <span v-else>&mdash;</span>
+  <div :class="alignmentClass" class="flex">
+    <ImageLoader
+      v-if="shouldShowLoader"
+      :src="imageUrl"
+      :max-width="field.maxWidth || field.indexWidth"
+      :rounded="field.rounded"
+      :aspect="field.aspect"
+    />
+    <span
+      v-if="usesCustomizedDisplay && !imageUrl"
+      class="break-words"
+      v-tooltip="field.value"
+    >
+      {{ field.displayedAs }}
+    </span>
+    <p
+      v-if="!usesCustomizedDisplay && !imageUrl"
+      :class="`text-${field.textAlign}`"
+      v-tooltip="field.value"
+    >
+      &mdash;
     </p>
+  </div>
 </template>
 
 <script>
+import { FieldValue } from 'laravel-nova'
+
 export default {
-    props: ['resourceName', 'field'],
+  mixins: [FieldValue],
+  props: ['viaResource', 'viaResourceId', 'resourceName', 'field'],
+
+  data: () => ({
+    loading: false,
+  }),
+
+  computed: {
+    shouldShowLoader() {
+      return this.imageUrl
+    },
+
+    imageUrl() {
+      return this.field?.previewUrl || this.field?.thumbnailUrl
+    },
+
+    alignmentClass() {
+      return {
+        left: 'items-center justify-start',
+        center: 'items-center justify-center',
+        right: 'items-center justify-end',
+      }[this.field.textAlign]
+    },
+  },
 }
 </script>
